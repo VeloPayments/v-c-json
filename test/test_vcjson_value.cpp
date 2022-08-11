3b85eb9a74ecf3a223c02d81b9dadcc70a6da9ad
Justin Handville <nanolith@gmail.com>
2022-08-11 12:03:04 -0400

    Create a value from a number.

--- test/test_vcjson_value.cpp
+++ test/test_vcjson_value.cpp
@@ -94,3 +94,43 @@ TEST(value_false_basics)
     resource_release(vcjson_value_resource_handle(value)); /* does nothing. */
     resource_release(allocator_resource_handle(alloc));
 }
+
+/**
+ * \brief Test for number related values.
+ */
+TEST(value_number_basics)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    vcjson_number* numberval = nullptr;
+    const double EXPECTED_VALUE = 99.25;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* create a number. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == vcjson_number_create(&numberval, alloc, EXPECTED_VALUE));
+
+    /* create a value from this number. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == vcjson_value_create_from_number(&value, alloc, numberval));
+
+    /* reset number value since it is now owned by the value. */
+    numberval = nullptr;
+
+    /* the type of this value is VCJSON_VALUE_TYPE_NUMBER. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_NUMBER == vcjson_value_type(value));
+
+    /* get the number value. */
+    TEST_ASSERT(STATUS_SUCCESS == vcjson_value_get_number(&numberval, value));
+
+    /* this number value is our expected value. */
+    TEST_EXPECT(EXPECTED_VALUE == vcjson_number_value(numberval));
+
+    /* clean up. */
+    resource_release(vcjson_value_resource_handle(value));
+    resource_release(allocator_resource_handle(alloc));
+}
