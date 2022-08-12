2885f0017bfe42ebb01fb299d8fb00714b1b86ce
Justin Handville <nanolith@gmail.com>
2022-08-12 01:41:01 -0400

    Add deep copy decls.

--- include/vcjson/vcjson.h
+++ include/vcjson/vcjson.h
@@ -105,13 +105,33 @@ status FN_DECL_MUST_CHECK
 vcjson_object_create(vcjson_object** obj, RCPR_SYM(allocator)* alloc);
 
 /**
+ * \brief Make a deep copy of the given object.
+ *
+ * \note On success, this function creates a  \ref vcjson_object instance. This
+ * is a resource that is owned by the caller. When no longer needed, this
+ * resource must be released by calling \ref resource_release on its resource
+ * handle.
+ *
+ * \param obj           Pointer to the object pointer to hold this value.
+ * \param alloc         The allocator to use for this operation.
+ * \param orig          The original object to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_object_copy(
+    vcjson_object** obj, RCPR_SYM(allocator)* alloc, const vcjson_object* orig);
+
+/**
  * \brief Get the number of elements in the given object instance.
  *
  * \param obj           The array instance for this operation.
  *
  * \returns the number of elements in this object instance.
  */
-size_t vcjson_object_elements(const vcjson_object* arr);
+size_t vcjson_object_elements(const vcjson_object* obj);
 
 /**
  * \brief Put a value into the given \ref vcjson_object instance.
@@ -297,6 +317,28 @@ vcjson_array_create(
     vcjson_array** arr, RCPR_SYM(allocator)* alloc, size_t size);
 
 /**
+ * \brief Make a deep copy of the given \ref vcjson_array.
+ *
+ * \note On success, this function creates a  \ref vcjson_array instance. This
+ * is a resource that is owned by the caller. When no longer needed, this
+ * resource must be released by calling \ref resource_release on its resource
+ * handle. By default, all values in this array will be initialized to \ref
+ * VCJSON_NULL. The caller should use \ref vcjson_array_set and \ref
+ * vcjson_array_get to change and get values in this array, respectfully.
+ *
+ * \param arr           Pointer to the array pointer to hold this value.
+ * \param alloc         The allocator to use for this operation.
+ * \param orig          The original array to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_array_copy(
+    vcjson_array** arr, RCPR_SYM(allocator)* alloc, const vcjson_array* orig);
+
+/**
  * \brief Get the size of the given array instance.
  *
  * \param arr           The array instance for this operation.
@@ -372,6 +414,27 @@ vcjson_number_create(
     vcjson_number** number, RCPR_SYM(allocator)* alloc, double value);
 
 /**
+ * \brief Make a deep copy of the given \ref vcjson_number instance.
+ *
+ * \note On success, this function creates a \ref vcjson_number instance. This
+ * is a resource that is owned by the caller. When no longer needed, this
+ * resource must be released by calling \ref resource_release on its resource
+ * handle.
+ *
+ * \param number        Pointer to the number pointer to hold this value.
+ * \param alloc         The allocator to use for this operation.
+ * \param orig          The original number to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_number_copy(
+    vcjson_number** number, RCPR_SYM(allocator)* alloc,
+    const vcjson_number* orig);
+
+/**
  * \brief Get the number value of this \ref vcjson_number instance.
  *
  * \param number        The number instance for this operation.
@@ -411,6 +474,27 @@ RCPR_SYM(resource)* vcjson_number_resource_handle(vcjson_number* number);
 status FN_DECL_MUST_CHECK
 vcjson_string_create(
     vcjson_string** string, RCPR_SYM(allocator)* alloc, const char* value);
+
+/**
+ * \brief Make a deep copy of the given \ref vcjson_string instance.
+ *
+ * \note On success, this function creates a \ref vcjson_string instance. This
+ * is a resource that is owned by the caller. When no longer needed, this
+ * resource must be released by calling \ref resource_release on its resource
+ * handle.
+ *
+ * \param string        Pointer to the string pointer to hold this value.
+ * \param alloc         The allocator to use for this operation.
+ * \param orig          The original string instance to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_string_copy(
+    vcjson_string** string, RCPR_SYM(allocator)* alloc,
+    const vcjson_string* orig);
 
 /**
  * \brief Get the string value of this \ref vcjson_string instance.
@@ -582,6 +666,26 @@ vcjson_value_create_from_true(
 status FN_DECL_MUST_CHECK
 vcjson_value_create_from_false(
     vcjson_value** value, RCPR_SYM(allocator)* alloc);
+
+/**
+ * \brief Create a deep copy of the given \ref vcjson_value instance.
+ *
+ * \note On success, this function creates a \ref vcjson_value instance. This
+ * value instance is a resource that is owned by the caller. When no longer
+ * needed, this resource must be released by calling \ref resource_release on
+ * its resource handle.
+ *
+ * \param value         Pointer to the value pointer to hold this value.
+ * \param alloc         The allocator to use for this operation.
+ * \param orig          The original value to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_value_copy(
+    vcjson_value** value, RCPR_SYM(allocator)* alloc, const vcjson_value* orig);
 
 /**
  * \brief Get the value type of this \ref vcjson_value instance.
