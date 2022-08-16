485caba5cdba0514be2e8a6b722ced7dde0f44ce
Justin Handville <nanolith@gmail.com>
2022-08-16 08:10:59 -0400

    Added vcjson_parse_string declaration.

--- include/vcjson/vcjson.h
+++ include/vcjson/vcjson.h
@@ -837,6 +837,29 @@ vcjson_parse(
     vcjson_value** value, size_t* error_begin, size_t* error_end,
     RCPR_SYM(allocator)* alloc, const char* input, size_t size);
 
+/**
+ * \brief Attempt to parse a JSON value from a UTF-8 string.
+ *
+ * \note This parse function must consume all input to be successful.
+ *
+ * \param value         Pointer to the value pointer to hold the JSON value on
+ *                      success.
+ * \param error_begin   Pointer to receive the start of an error location on
+ *                      failure.
+ * \param error_end     Pointer to receive the end of an error location on
+ *                      failure.
+ * \param alloc         The allocator to use for this operation.
+ * \param input         The UTF-8 input string.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status FN_DECL_MUST_CHECK
+vcjson_parse_string(
+    vcjson_value** value, size_t* error_begin, size_t* error_end,
+    RCPR_SYM(allocator)* alloc, const char* input);
+
 #if defined(__cplusplus)
 }
 #endif /* defined(__cplusplus) */
