c0369073770385b953c6952b0a462d641a90e26e
Justin Handville <nanolith@gmail.com>
2022-08-20 03:22:57 -0400

    Added parsing for true, false, and null.

--- src/vcjson_parse.c
+++ src/vcjson_parse.c
@@ -11,6 +11,22 @@
 
 #include "vcjson_internal.h"
 
+RCPR_IMPORT_allocator;
+
+/* forward decls. */
+static status
+    vcjson_parse_value_true(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset);
+static status
+    vcjson_parse_value_false(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset);
+static status
+    vcjson_parse_value_null(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset);
+
 /**
  * \brief Attempt to parse a JSON value from a UTF-8 character buffer.
  *
@@ -35,12 +51,212 @@ vcjson_parse(
     vcjson_value** value, size_t* error_begin, size_t* error_end,
     RCPR_SYM(allocator)* alloc, const char* input, size_t size)
 {
-    (void)value;
-    (void)error_begin;
-    (void)error_end;
-    (void)alloc;
-    (void)input;
-    (void)size;
+    status retval;
+    int symbol;
+    size_t startpos, endpos, offset;
 
-    return -1;
+    (void)value;
+    (void)alloc;
+
+    /* initialize the positions as 0 to start the parse. */
+    startpos = endpos = offset = 0;
+
+    /* scan for a symbol. */
+    retval =
+        vcjson_scan_symbol(
+            &symbol, &startpos, &endpos, input, size, &offset);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* set the error positions. */
+    *error_begin = startpos;
+    *error_end = endpos;
+
+    /* decode the symbol received. */
+    switch (symbol)
+    {
+        /* we need a valid JSON value for this parse to be successful. */
+        case VCJSON_LEXER_SYMBOL_SPECIAL_EOF:
+            return ERROR_VCJSON_PARSE_b369f991_4e11_4210_9076_ddc799d5bf44;
+
+        /* the true value was parsed. */
+        case VCJSON_LEXER_SYMBOL_TRUE:
+            return
+                vcjson_parse_value_true(
+                    value, error_begin, error_end, alloc, input, size, &offset);
+
+        /* the false value was parsed. */
+        case VCJSON_LEXER_SYMBOL_FALSE:
+            return
+                vcjson_parse_value_false(
+                    value, error_begin, error_end, alloc, input, size, &offset);
+
+        /* the null value was parsed. */
+        case VCJSON_LEXER_SYMBOL_NULL:
+            return
+                vcjson_parse_value_null(
+                    value, error_begin, error_end, alloc, input, size, &offset);
+
+        /* an unknown symbol was encountered. */
+        default:
+            return ERROR_VCJSON_PARSE_fb48555e_2ed9_414a_841e_0d5b39b52090;
+    }
+}
+
+/**
+ * \brief Create a true value and verify that we have consumed all input.
+ *
+ * \param value         Pointer to the value pointer to hold the JSON value on
+ *                      success.
+ * \param error_begin   Pointer to receive the start of an error location on
+ *                      failure.
+ * \param error_end     Pointer to receive the end of an error location on
+ *                      failure.
+ * \param alloc         The allocator to use for this operation.
+ * \param input         The input UTF-8 character buffer.
+ * \param size          The size of this buffer.
+ * \param offset        The current offset in the input buffer.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static status
+    vcjson_parse_value_true(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset)
+{
+    status retval;
+    int symbol;
+
+    /* scan the next token. */
+    retval =
+        vcjson_scan_symbol(
+            &symbol, error_begin, error_end, input, size, offset);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* verify that this token is EOF. */
+    if (VCJSON_LEXER_SYMBOL_SPECIAL_EOF != symbol)
+    {
+        return ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402;
+    }
+
+    /* create the true instance. */
+    retval = vcjson_value_create_from_true(value, alloc);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* success. */
+    return STATUS_SUCCESS;
+}
+
+/**
+ * \brief Create a false value and verify that we have consumed all input.
+ *
+ * \param value         Pointer to the value pointer to hold the JSON value on
+ *                      success.
+ * \param error_begin   Pointer to receive the start of an error location on
+ *                      failure.
+ * \param error_end     Pointer to receive the end of an error location on
+ *                      failure.
+ * \param alloc         The allocator to use for this operation.
+ * \param input         The input UTF-8 character buffer.
+ * \param size          The size of this buffer.
+ * \param offset        The current offset in the input buffer.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static status
+    vcjson_parse_value_false(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset)
+{
+    status retval;
+    int symbol;
+
+    /* scan the next token. */
+    retval =
+        vcjson_scan_symbol(
+            &symbol, error_begin, error_end, input, size, offset);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* verify that this token is EOF. */
+    if (VCJSON_LEXER_SYMBOL_SPECIAL_EOF != symbol)
+    {
+        return ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402;
+    }
+
+    /* create the false instance. */
+    retval = vcjson_value_create_from_false(value, alloc);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* success. */
+    return STATUS_SUCCESS;
+}
+
+/**
+ * \brief Create a null value and verify that we have consumed all input.
+ *
+ * \param value         Pointer to the value pointer to hold the JSON value on
+ *                      success.
+ * \param error_begin   Pointer to receive the start of an error location on
+ *                      failure.
+ * \param error_end     Pointer to receive the end of an error location on
+ *                      failure.
+ * \param alloc         The allocator to use for this operation.
+ * \param input         The input UTF-8 character buffer.
+ * \param size          The size of this buffer.
+ * \param offset        The current offset in the input buffer.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static status
+    vcjson_parse_value_null(
+        vcjson_value** value, size_t* error_begin, size_t* error_end,
+        allocator* alloc, const char* input, size_t size, size_t* offset)
+{
+    status retval;
+    int symbol;
+
+    /* scan the next token. */
+    retval =
+        vcjson_scan_symbol(
+            &symbol, error_begin, error_end, input, size, offset);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* verify that this token is EOF. */
+    if (VCJSON_LEXER_SYMBOL_SPECIAL_EOF != symbol)
+    {
+        return ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402;
+    }
+
+    /* create the null instance. */
+    retval = vcjson_value_create_from_null(value, alloc);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* success. */
+    return STATUS_SUCCESS;
 }
--- test/test_vcjson_parse.cpp
+++ test/test_vcjson_parse.cpp
@@ -14,7 +14,6 @@ RCPR_IMPORT_resource;
 
 TEST_SUITE(vcjson_parse);
 
-#if 0
 /**
  * Verify that an empty string causes an error.
  */
@@ -22,8 +21,8 @@ TEST(vcjson_parse_empty_string)
 {
     allocator* alloc = nullptr;
     vcjson_value* value = nullptr;
-    size_t error_begin = 0;
-    size_t error_end = 0;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
 
     /* create a malloc allocator. */
     TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
@@ -34,8 +33,299 @@ TEST(vcjson_parse_empty_string)
             == vcjson_parse_string(
                     &value, &error_begin, &error_end, alloc, ""));
 
+    /* when an error is encountered, the error positions are set, and value is
+     * NULL. */
+    TEST_EXPECT(0 == error_begin);
+    TEST_EXPECT(0 == error_end);
+    TEST_EXPECT(nullptr == value);
+
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS
         == resource_release(allocator_resource_handle(alloc)));
 }
-#endif
+
+/**
+ * Verify that we can parse JSON true.
+ */
+TEST(vcjson_parse_true)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    vcjson_bool* boolvalue = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "true"));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is boolean. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_BOOL == vcjson_value_type(value));
+    /* verify that this value is true. */
+    TEST_ASSERT(STATUS_SUCCESS == vcjson_value_get_bool(&boolvalue, value));
+    TEST_EXPECT(VCJSON_TRUE == boolvalue);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can parse JSON true with whitespace surrounding it.
+ */
+TEST(vcjson_parse_true_whitespace)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    vcjson_bool* boolvalue = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "  true\n\t "));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is boolean. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_BOOL == vcjson_value_type(value));
+    /* verify that this value is true. */
+    TEST_ASSERT(STATUS_SUCCESS == vcjson_value_get_bool(&boolvalue, value));
+    TEST_EXPECT(VCJSON_TRUE == boolvalue);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that parsing true with any other symbol after it causes an error.
+ */
+TEST(vcjson_parse_true_true)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing multiple symbols fails. */
+    TEST_EXPECT(
+        ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "true true"));
+
+    /* the error begin / end should point to the second symbol. */
+    TEST_EXPECT(5 == error_begin);
+    TEST_EXPECT(8 == error_end);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can parse JSON false.
+ */
+TEST(vcjson_parse_false)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    vcjson_bool* boolvalue = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "false"));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is boolean. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_BOOL == vcjson_value_type(value));
+    /* verify that this value is true. */
+    TEST_ASSERT(STATUS_SUCCESS == vcjson_value_get_bool(&boolvalue, value));
+    TEST_EXPECT(VCJSON_FALSE == boolvalue);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can parse JSON false with whitespace surrounding it.
+ */
+TEST(vcjson_parse_false_whitespace)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    vcjson_bool* boolvalue = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "  false\n\t "));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is boolean. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_BOOL == vcjson_value_type(value));
+    /* verify that this value is true. */
+    TEST_ASSERT(STATUS_SUCCESS == vcjson_value_get_bool(&boolvalue, value));
+    TEST_EXPECT(VCJSON_FALSE == boolvalue);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that parsing false with any other symbol after it causes an error.
+ */
+TEST(vcjson_parse_false_true)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing multiple symbols fails. */
+    TEST_EXPECT(
+        ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "false true"));
+
+    /* the error begin / end should point to the second symbol. */
+    TEST_EXPECT(6 == error_begin);
+    TEST_EXPECT(9 == error_end);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can parse JSON null.
+ */
+TEST(vcjson_parse_null)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "null"));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is null. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_NULL == vcjson_value_type(value));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can parse JSON null with whitespace surrounding it.
+ */
+TEST(vcjson_parse_null_whitespace)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing succeeds. */
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "  null\n\t "));
+
+    /* verify that the value is set. */
+    TEST_ASSERT(nullptr != value);
+    /* verify that the value type is null. */
+    TEST_EXPECT(VCJSON_VALUE_TYPE_NULL == vcjson_value_type(value));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(vcjson_value_resource_handle(value)));
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that parsing null with any other symbol after it causes an error.
+ */
+TEST(vcjson_parse_null_true)
+{
+    allocator* alloc = nullptr;
+    vcjson_value* value = nullptr;
+    size_t error_begin = 0xffff;
+    size_t error_end = 0xffff;
+
+    /* create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* parsing multiple symbols fails. */
+    TEST_EXPECT(
+        ERROR_VCJSON_PARSE_b87aa047_46c8_453c_aa3c_bb7c1dd70402
+            == vcjson_parse_string(
+                    &value, &error_begin, &error_end, alloc, "null true"));
+
+    /* the error begin / end should point to the second symbol. */
+    TEST_EXPECT(5 == error_begin);
+    TEST_EXPECT(8 == error_end);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS
+        == resource_release(allocator_resource_handle(alloc)));
+}
